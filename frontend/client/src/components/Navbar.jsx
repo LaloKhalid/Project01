e8666eb754a50da1bf981d6e26bec450c51b4cc2
LarebKhalid <lareb.usman@yahoo.com>
2026-09-22 22:04:30 +0200

    logout function

--- frontend/client/src/components/Navbar.jsx
+++ frontend/client/src/components/Navbar.jsx
@@ -1,13 +1,22 @@
-import { NavLink } from "react-router-dom";
+import { NavLink, useNavigate } from "react-router-dom";
 import "./navbar.css";
 
 function Navbar() {
+
+      const navigate = useNavigate();
+
+      const handleLogout = () => {
+        localStorage.removeItem("token");
+        navigate("/auth");    
+      }
   return (
     <nav className="navbar">
       <div className="navbar-links">
         <NavLink to="/" end>Dashboard</NavLink>
         <NavLink to="/applications">Applications</NavLink>
-        <NavLink to="/companies">Companies</NavLink>
+        <button onClick={handleLogout} className="navbar-logout-btn">
+          Logout
+        </button>
         <NavLink to="/auth">Auth</NavLink>
       </div>
     </nav>
